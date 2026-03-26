cadc716f15a9ffb3641b48f35e41291229d0fff7
d0nk0tr0n <59426312+d0nk0tr0n@users.noreply.github.com>
2026-03-26 12:08:00 -0700

    Consistency in logging and explicitly targeting pm.py

--- resources/lib/main.py
+++ resources/lib/main.py
@@ -1,10 +1,7 @@
 import os
+import xbmc
 import xbmcaddon
 import xbmcgui
-
-def log(*args):
-    message = 'LPG: ' + ' '.join(str(a.decode('utf-8') if isinstance(a, bytes) else a) for a in args)
-    xbmc.log(msg=message, level=xbmc.LOGDEBUG)
 
 def run():
     addon = xbmcaddon.Addon()
@@ -13,8 +10,8 @@ def run():
     if os.path.exists(process):
         if xbmcgui.Dialog().yesno("Last.fm Playlist Generator", "Would you like to stop last.fm playlist generator?"):
             os.remove(process)
-            log("default os.remove")
+            xbmc.log(msg="LPM: default os.remove", level=xbmc.LOGDEBUG)
     else:
         with open(process, 'w') as f:
             f.write(str(os.getpid()))
-        import pm
+        from resources.lib import pm

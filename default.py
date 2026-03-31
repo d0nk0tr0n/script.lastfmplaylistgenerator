332d352572997d60438d1ad807ce072a890624b3
d0nk0tr0n <59426312+d0nk0tr0n@users.noreply.github.com>
2026-03-30 18:24:28 -0700

    Removed xbmc, blank lines, __addon__, __cwd__, BASE_RESOURCE_PATH — inlined the path directly into the process assignment

--- default.py
+++ default.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-import os 
-import xbmc
+import os
 import xbmcgui
 import xbmcaddon
-from traceback import print_exc
 
-__addon__        = xbmcaddon.Addon()
-__addonversion__ = __addon__.getAddonInfo('version')
-__cwd__          = __addon__.getAddonInfo('path')
-BASE_RESOURCE_PATH = os.path.join( __cwd__, "resources" )
-process = os.path.join( BASE_RESOURCE_PATH , "pm.pid")
-
+process = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), "resources", "pm.pid")
 if os.path.exists(process):
-    if xbmcgui.Dialog().yesno("Last.FM playlist generator", "Would you like to stop Last.FM playlist generator?" ):
-        os.remove(process)        
+    if xbmcgui.Dialog().yesno("Last.FM playlist generator", "Would you like to stop Last.FM playlist generator?"):
+        os.remove(process)
 else:
     with open(process, "w") as f:
         f.write(str(os.getpid()))
